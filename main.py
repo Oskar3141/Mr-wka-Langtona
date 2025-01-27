77617f789e82c3d02c0db05cfaf26481e672c582
Oskar <62214403+Oskar3141@users.noreply.github.com>
2025-01-27 19:33:21 +0100

    Plansza v3

--- main.py
+++ main.py
@@ -11,34 +11,35 @@ class Gra:
 	def __init__(self, s: int, w: int):
 		self.planszaS: int = s;
 		self.planszaW: int = w;
+		self.mrowkaI = self.planszaS * self.planszaW // 2;
+		self.mrowkaI += 0 if self.planszaS * self.planszaW % 2 == 1 else self.planszaW // 2;
 		
-		self.plansza: [[Pole]] = [[Pole(False, False) for _ in range(0, self.planszaS)] for _ in range(0, self.planszaW)];
-		self.plansza[self.planszaW // 2][self.planszaS // 2].mrowka = True;
+		self.plansza: [Pole] = [Pole(False, False) for _ in range(0, self.planszaS * self.planszaW)];
+		self.plansza[self.mrowkaI].mrowka = True;
 
 	def wyswietlPlansze(self):
 		os.system("cls" if os.name == "nt" else "clear");
-		for y in self.plansza:
-			for x in y:
-				znak: str = "";
-				if x.mrowka:
-					znak = "@"
-				elif x.zywe:
-					znak = "o"
-				else:
-					znak = "."
+		for i in range(0, self.planszaS * self.planszaW):
+			znak: str = "";
+			if self.plansza[i].mrowka:
+				znak = "@"
+			elif self.plansza[i].zywe:
+				znak = "o"
+			else:
+				znak = "."
 
-				print(znak, " ", end="");
-			print("");
+			print(znak, " ", end="");
+			if i != 0 and (i+1) % self.planszaW == 0:
+				print("");
 
 	def zaktualizujPlansze(self):
-		# coś...
-		odwalsie = True;
+		doodsod = 2;
 
 	def graj(self):
 		while True:
-			self.zaktualizujPlane();
+			self.zaktualizujPlansze();
 			self.wyswietlPlansze();
 			time.sleep(0.5);
 
-gra: Gra = Gra(20, 20);
+gra: Gra = Gra(21, 21);
 gra.graj();
