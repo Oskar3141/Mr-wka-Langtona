40efc7479fa0043a61530c803b6d985234660ac7
Oskar <62214403+Oskar3141@users.noreply.github.com>
2025-01-27 19:50:42 +0100

    Aktualizowanie pozycji mrówki, liczenie kroków

--- main.py
+++ main.py
@@ -8,38 +8,62 @@ class Pole:
 		self.mrowka = mrowka;
 
 class Gra:
-	def __init__(self, s: int, w: int):
+	def __init__(self, s: int, w: int) -> None:
+		self.kroki: int = 0;
+		self.czasBezAktualizacji: float = 0.1;
 		self.planszaS: int = s;
 		self.planszaW: int = w;
-		self.mrowkaI = self.planszaS * self.planszaW // 2;
+		self.mrowkaI: int = self.planszaS * self.planszaW // 2;
 		self.mrowkaI += 0 if self.planszaS * self.planszaW % 2 == 1 else self.planszaW // 2;
+		self.mrowkaKier: int = 0;
 		
 		self.plansza: [Pole] = [Pole(False, False) for _ in range(0, self.planszaS * self.planszaW)];
 		self.plansza[self.mrowkaI].mrowka = True;
 
-	def wyswietlPlansze(self):
+	def wyswietlPlansze(self) -> None:
 		os.system("cls" if os.name == "nt" else "clear");
 		for i in range(0, self.planszaS * self.planszaW):
 			znak: str = "";
 			if self.plansza[i].mrowka:
 				znak = "@"
 			elif self.plansza[i].zywe:
-				znak = "o"
+				znak = "O"
 			else:
 				znak = "."
 
 			print(znak, " ", end="");
 			if i != 0 and (i+1) % self.planszaW == 0:
 				print("");
+		print("Kroki: ", self.kroki);
 
-	def zaktualizujPlansze(self):
-		doodsod = 2;
+	def zaktualizujPlansze(self) -> None:
+		if self.plansza[self.mrowkaI].zywe:
+			self.mrowkaKier += 1;
+			self.mrowkaKier = 0 if self.mrowkaKier == 4 else self.mrowkaKier;
+		else:
+			self.mrowkaKier -= 1;
+			self.mrowkaKier = 3 if self.mrowkaKier == -1 else self.mrowkaKier; 
 
-	def graj(self):
+		self.plansza[self.mrowkaI].mrowka = False;
+		self.plansza[self.mrowkaI].zywe = not self.plansza[self.mrowkaI].zywe;
+
+		if self.mrowkaKier == 0:
+			self.mrowkaI -= self.planszaS;
+		elif self.mrowkaKier == 1:
+			self.mrowkaI += 1;			
+		elif self.mrowkaKier == 2:
+			self.mrowkaI += self.planszaS;
+		else:
+			self.mrowkaI -= 1;
+
+		self.plansza[self.mrowkaI].mrowka = True;
+		self.kroki += 1;
+
+	def graj(self) -> None:
 		while True:
 			self.zaktualizujPlansze();
 			self.wyswietlPlansze();
-			time.sleep(0.5);
+			time.sleep(self.czasBezAktualizacji);
 
-gra: Gra = Gra(21, 21);
+gra: Gra = Gra(9, 9);
 gra.graj();
